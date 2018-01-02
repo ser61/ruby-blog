9ac998cbf5b5ca719140a6c8fcaf8f98c1d6677b
SergioW <sergioweimarzapata@gmail.com>
2018-01-01 21:27:52 -0400

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'articles/index'
+
   root 'welcome#index'
+
+  resource :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
